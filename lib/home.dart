96651caeca281e345a8cfa0f64d3ebcf5d1d4814
Thiru <t.sadaiyappan@lakeba.com>
2021-04-11 17:49:53 +0530

    Container Updates

--- lib/home.dart
+++ lib/home.dart
@@ -4,18 +4,27 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text(
-        "My simple Content",
-        style: TextStyle(
-            fontSize: 24,
-            fontWeight: FontWeight.bold,
-            fontStyle: FontStyle.italic,
-            color: Colors.amber,
-            decoration: TextDecoration.underline,
-            backgroundColor: Colors.red,
-            wordSpacing: 20,
-            letterSpacing: 8,
-            shadows: [Shadow(color: Colors.black, offset: Offset(8, 4))]),
+      child: Container(
+        width: 250,
+        height: 500,
+        alignment: Alignment(0.0, -0.5),
+        decoration: BoxDecoration(
+            color: Colors.yellow,
+            border: Border.all(width: 5, color: Colors.amber),
+            borderRadius: BorderRadius.circular(10)),
+        child: Text(
+          "My simple Content",
+          style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+              fontStyle: FontStyle.italic,
+              color: Colors.amber,
+              decoration: TextDecoration.underline,
+              backgroundColor: Colors.red,
+              wordSpacing: 20,
+              letterSpacing: 8,
+              shadows: [Shadow(color: Colors.black, offset: Offset(8, 4))]),
+        ),
       ),
     );
   }
